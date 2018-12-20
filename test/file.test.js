90dab923d9b9b11ac777239f6dac7e8818e645fc
Jared Hanson <jaredhanson@gmail.com>
2018-12-19 17:03:12 -0800

    Implement File with data parsing support.

---
+++ test/file.test.js
@@ -0,0 +1,47 @@
+var File = require('../lib/file');
+  
+  
+describe('File', function () {
+  
+  it('should export constructor', function () {
+    expect(File).to.be.a('function');
+  });
+  
+  describe('constructor', function () {
+    
+    it('should construct with object', function () {
+      var file = new File('config.yaml');
+      expect(file.toObject()).to.deep.equal({});
+    });
+    
+  }); // constructor
+  
+  describe.only('#open', function () {
+    
+    it('should read JSON file', function () {
+      var file = new File('test/fixtures/settings.json');
+      file.open();
+      expect(file.toObject()).to.deep.equal({ server: { host: 'www.example.com', port: 8080 } });
+    });
+    
+    it('should read JSON5 file', function () {
+      var file = new File('test/fixtures/settings.json5');
+      file.open();
+      expect(file.toObject()).to.deep.equal({ server: { host: 'www.example.com', port: 8080 } });
+    });
+    
+    it('should read YAML file', function () {
+      var file = new File('test/fixtures/settings.yaml');
+      file.open();
+      expect(file.toObject()).to.deep.equal({ server: { host: 'www.example.com', port: 8080 } });
+    });
+    
+    it('should read TOML file', function () {
+      var file = new File('test/fixtures/settings.toml');
+      file.open();
+      expect(file.toObject()).to.deep.equal({ server: { host: 'www.example.com', port: 8080 } });
+    });
+    
+  }); // #open
+  
+});
