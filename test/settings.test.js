d57b9213904e9f6459608199c74c4a0656276331
Jared Hanson <jaredhanson@gmail.com>
2018-12-29 08:23:39 -0800

    Add tests.

--- test/settings.test.js
+++ test/settings.test.js
@@ -30,6 +30,27 @@ describe('Settings', function () {
       expect(settings._sources).to.have.length(2);
     });
     
-  });
+  }); // #read
+  
+  describe('#get', function () {
+    
+    it('should read from single source', function () {
+      var settings = new Settings();
+      settings.use(new Object({ host: 'www.example.com' }));
+      
+      expect(settings.get('host')).to.equal('www.example.com');
+      expect(settings.get('port')).to.be.undefined;
+    });
+    
+    it('should read from multiple sources', function () {
+      var settings = new Settings();
+      settings.use(new Object({ host: 'www.example.net' }));
+      settings.use(new Object({ host: 'www.example.com', port: 8080 }));
+      
+      expect(settings.get('host')).to.equal('www.example.net');
+      expect(settings.get('port')).to.equal(8080);
+    });
+    
+  }); // #get
   
 });
